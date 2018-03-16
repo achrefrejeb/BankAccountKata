05ae30a9682b313c9a14df0273c07374ea4742cd
Achref REJEB <arejeb@nexeo.local>
2018-03-16 17:35:00 +0100

    first commit

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountKata
+{
+    public class BankAccount
+    {
+        private double _balance;
+
+        private List<Operation> _operations;
+
+        public BankAccount(double balance)
+        {
+            this._balance = balance;
+            this._operations = new List<Operation>();
+        }
+
+        public void Deposit(double value)
+        {
+            this._balance += value;
+            this._operations.Add(new Operation(OpearationType.Deposit,DateTime.Now, value,this._balance));
+        }
+
+        public void Withdrawal(double value)
+        {
+            this._balance -= value;
+            this._operations.Add(new Operation(OpearationType.Withdrawal, DateTime.Now, value, this._balance));
+        }
+
+        public double GetBalance()
+        {
+            return this._balance;
+        }
+
+        public string GetOperations()
+        {
+            StringBuilder operationResults = new StringBuilder("Account History\n---------------------------\n");
+            foreach (Operation operation in _operations)
+            {
+                operationResults.AppendLine(string.Format("Operation: {0}",operation.OpearationType.ToString()));
+                operationResults.AppendLine(string.Format("Date: {0}",operation.Date.ToString("d")));
+                operationResults.AppendLine(string.Format("Amount: {0}",operation.Amount));
+                operationResults.AppendLine(string.Format("Account Balance: {0}",operation.Balance));
+                operationResults.AppendLine("---------------------------");
+            }
+
+            return operationResults.ToString();
+        }
+    }
+
+    public class Operation
+    {
+        public OpearationType OpearationType;
+        public DateTime Date;
+        public double Amount;
+        public double Balance;
+
+        public Operation(OpearationType operationtype, DateTime date, double amount, double balance)
+        {
+            this.OpearationType = operationtype;
+            this.Date = date;
+            this.Amount = amount;
+            this.Balance = balance;
+        }
+    }
+
+    public enum OpearationType
+    {
+        Deposit,
+        Withdrawal
+    }
+}
---
+++ BankAccountTest/BankAccountTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Markup;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using BankAccountKata;
+using NUnit.Framework;
+
+namespace BankAccountTest
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        [TestCase(0d, 100d, ExpectedResult = 100d)]
+        [TestCase(100d, 100d, ExpectedResult = 200d)]
+        [TestCase(0d, 200d, ExpectedResult = 200d)]
+        [TestCase(200d, 100d, ExpectedResult = 300d)]
+        public double ShouldReturnGratherSumWhenMakeDeposit(double balance, double value)
+        {
+            BankAccount account = new BankAccount(balance);
+            account.Deposit(value);
+            return account.GetBalance();
+        }
+
+        [TestMethod]
+        [TestCase(100d, 100d, ExpectedResult = 0d)]
+        [TestCase(200d, 100d, ExpectedResult = 100d)]
+        [TestCase(100d, 0d, ExpectedResult = 100d)]
+        [TestCase(300d, 100d, ExpectedResult = 200d)]
+        public double ShouldReturnLowerSumWhenMakeWithdrawal(double balance, double value)
+        {
+            BankAccount account = new BankAccount(balance);
+            account.Withdrawal(value);
+            return account.GetBalance();
+        }
+
+        [TestMethod]
+        public void ShouldReturnListOfOperationsWhenNeeded(  )
+        {
+            double value = 100d;
+            BankAccount account = new BankAccount(0d);
+            account.Deposit(value);
+            account.Withdrawal(value);
+            var list = account.GetOperations();
+            Check.That(list).Equals(string.Format("Account History\n---------------------------\nOperation: Deposit\r\nDate: {0}\r\nAmount: 100\r\nAccount Balance: 100\r\n---------------------------\r\nOperation: Withdrawal\r\nDate: {0}\r\nAmount: 100\r\nAccount Balance: 0\r\n---------------------------\r\n", DateTime.Now.Date.ToString("d")));
+        }
+    }
+}
